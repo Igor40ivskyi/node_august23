732e9c3e64af841a3b3d8574bf423f4b4e77c4c9
Admin <sorokivskyi777@gmail.com>
2023-08-26 12:59:26 +0300

    lesson 3 ts eslint

--- src/app.ts
+++ src/app.ts
@@ -1,95 +1,80 @@
 import express, { Request, Response } from "express";
 
+const users = [
+  {
+    name: "Andriy",
+    age: 30,
+    gender: "male",
+  },
+  {
+    name: "Vasya",
+    age: 22,
+    gender: "male",
+  },
+  {
+    name: "Ann",
+    age: 13,
+    gender: "female",
+  },
+  {
+    name: "Monica",
+    age: 25,
+    gender: "female",
+  },
+  {
+    name: "Ivan",
+    age: 40,
+    gender: "male",
+  },
+];
+
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.get("/users", async (req: Request, res: Response) => {
-  const users = await fileService.readDB();
-  res.json(users);
+app.get("/users", (req: Request, res: Response) => {
+  console.log("HELLO FROM APP GET /");
+  res.status(200).json(users);
 });
 
-app.get("/users/:userId", async (req: Request, res: Response) => {
+app.get("/users/:userId", (req: Request, res: Response) => {
+  console.log(req.params);
+  const { userId } = req.params;
+  res.status(200).json(users[+userId]);
+});
+
+app.post("/users", (req: Request, res: Response) => {
+  users.push(req.body);
+
+  res.status(201).json({
+    message: "User created!",
+  });
+});
+
+app.put("/users/:userId", (req: Request, res: Response) => {
   const { userId } = req.params;
 
-  const users = await fileService.readDB();
+  console.log(users);
 
-  const user = users.find((user: any) => user.id === +userId);
-  if (!user) {
-    res.status(422).json("user not found");
-  }
+  users[+userId] = req.body;
 
-  res.json(user);
+  console.log(users);
+
+  res.status(201).json({
+    message: "User updated!",
+    data: users[+userId],
+  });
 });
 
-app.post("/users", async (req: Request, res: Response) => {
-  const { name, age } = req.body;
-
-  if (!name || name.length < 3) {
-    return res.status(400).json("name is not correct");
-  }
-
-  if (!age || age < 18 || age > 100) {
-    return res.status(400).json("age is not correct");
-  }
-
-  const users = await fileService.readDB();
-
-  const newUser = {
-    id: users.length ? users.length + 1 : 1,
-    name,
-    age,
-  };
-
-  users.push(newUser);
-
-  await fileService.writeDB(users);
-
-  res.status(201).json(newUser);
-});
-
-app.patch("/users/:userId", async (req: Request, res: Response) => {
-  const { userId } = req.params;
-  const { name, age } = req.body;
-
-  if (name && name.length < 3) {
-    return res.status(400).json("name is not correct");
-  }
-
-  if (age && (age < 18 || age > 100)) {
-    return res.status(400).json("age is not correct");
-  }
-
-  const users = await fileService.readDB();
-  const user = users.find((user: any) => user.id === +userId);
-
-  if (!user) {
-    return res.status(422).json("user not found");
-  }
-
-  if (name) user.name = name;
-  if (age) user.age = age;
-
-  await fileService.writeDB(users);
-
-  res.status(201).json(user);
-});
-
-app.delete("/users/:userId", async (req: Request, res: Response) => {
+app.delete("/users/:userId", (req: Request, res: Response) => {
   const { userId } = req.params;
 
-  const users = await fileService.readDB();
-  const index = users.findIndex((user: any) => user.id === +userId);
+  users.splice(+userId, 1);
 
-  if (index === -1) {
-    return res.status(422).json("user not found");
-  }
-
-  users.splice(index, 1);
-
-  await fileService.writeDB(users);
-  res.sendStatus(204);
+  res.status(200).json({
+    message: "User deleted!",
+  });
 });
 
 const PORT = 5001;
