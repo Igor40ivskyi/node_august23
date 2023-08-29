e50a8374e9a8049fb9892b6ee4eba192ccd5d5a7
Admin <sorokivskyi777@gmail.com>
2023-08-29 17:01:43 +0300

    lesson4 mongoose

---
+++ src/validators/user.validator.ts
@@ -0,0 +1,11 @@
+import Joi from "joi";
+import {EGenders} from "../enums/user.enum";
+
+export class UserValidator {
+  create = Joi.object({
+      firstName: Joi.string().min(3).max(30).trim(),
+      age: Joi.number().min(1).max(199),
+      gender: Joi.valid(EGenders),
+      email:Joi.string().regex().lowercase()
+  });
+}
