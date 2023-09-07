a836f495c00cac675dfa08c7f2e9b2915da6e80e
Admin <sorokivskyi777@gmail.com>
2023-09-07 12:37:42 +0300

    lesson7 nodemailer learning

--- src/services/email.service.ts
+++ src/services/email.service.ts
@@ -1,4 +1,6 @@
 import nodemailer from "nodemailer";
+import hbs from "nodemailer-express-handlebars";
+import * as path from "path";
 
 class EmailService {
   private transporter;
@@ -11,14 +13,38 @@ class EmailService {
         pass: "ajzadmzqpixkaagk",
       },
     });
+
+    const hbsOptions = {
+      viewEngine: {
+        extname: ".hbs",
+        defaultLayout: "main",
+        layoutsDir: path.join(
+          process.cwd(),
+          "src",
+          "email_templates",
+          "layouts",
+        ),
+        partialsDir: path.join(
+          process.cwd(),
+          "src",
+          "email_templates",
+          "partials",
+        ),
+      },
+      viewPath: path.join(process.cwd(), "src", "email_templates", "views"),
+      extName: ".hbs",
+    };
+
+    this.transporter.use("compile", hbs(hbsOptions));
   }
 
   public async sendEmail(email: string) {
-    return this.transporter.sendMail({
+    const mailOptiong = {
       to: email,
       subject: "Hello it is my first email",
-      html: "<div>XXXXXXXXXXXXXXXXXXXXXXXXXXXXXX</div>",
-    });
+      template: "register",
+    };
+    return await this.transporter.sendMail(mailOptiong);
   }
 }
 
